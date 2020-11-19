1bd60e47c3e02c5a0dc375b336835b41a7a2084c
Walter Setti <walter.setti@iit.it>
2020-11-19 15:39:08 +0100

    Traffic Jam

---
+++ Audiospatial/Secondo_Scenario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Audiospatial
+{
+    public partial class Secondo_Scenario : UserControl
+    {
+        public Main parentForm { get; set; }
+        public Secondo_Scenario()
+        {
+            InitializeComponent();
+           // this.BackgroundImage = Properties.Resources.bed;
+            //this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+        }
+
+        private void Secondo_Scenario_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
